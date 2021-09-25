b51c1afa7e1ccb5b048b375837b7680169708a7a
aoi0810 <gtmwaoi0810@gmil.com>
2021-09-25 11:14:37 +0900

    Initialize repository

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Category.find_or_create_by(name: "Work")
+Category.find_or_create_by(name: "Life")
+Category.find_or_create_by(name: "Hobby")
+Category.find_or_create_by(name: "Other")
+Category.find_or_create_by(name: "Programming")
